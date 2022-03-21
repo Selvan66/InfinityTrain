d5d006475d9375e9dfd9d47b350da4ad0fb2211d
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-03-21 11:55:36 +0100

    change achievements to statistic

--- code/States/MenuOptionsState.cpp
+++ code/States/MenuOptionsState.cpp
@@ -51,8 +51,8 @@ void MenuOptionsState::createButtons()
 	
 	mTextButtons.emplace_back(context);
 	auto& achievementsButton = mTextButtons.back();
-	achievementsButton.setText("ACHIEVEMENTS");
-	achievementsButton.setCallback([](){std::cout << "ACHIEVEMENTS" << std::endl;});
+	achievementsButton.setText("STATISTICS");
+	achievementsButton.setCallback([](){std::cout << "STATISTICS" << std::endl;});
 	achievementsButton.setPosition(sf::Vector2f(window_size.x / 2, window_size.y / 2 + buttonHeight));
 
 
