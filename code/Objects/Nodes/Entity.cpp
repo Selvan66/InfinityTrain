e0e34195cfa50226e84b92f4dafa0b76da7c84bf
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-04-03 09:56:53 +0200

    Entity node

---
+++ code/Objects/Nodes/Entity.cpp
@@ -0,0 +1,70 @@
+/** @file Entity.cpp */
+#include "Objects/Nodes/Entity.h"
+
+Entity::Entity(int hitpoints)
+: mVelocity()
+, mHitpoints(hitpoints)
+{ }
+
+void Entity::setVelocity(sf::Vector2f velocity)
+{
+    mVelocity = velocity;  
+}
+
+void Entity::setVelocity(float vx, float vy)
+{
+    mVelocity = sf::Vector2f(vx, vy);
+}
+
+void Entity::accelerate(sf::Vector2f velocity)
+{
+    mVelocity += velocity;
+}
+
+void Entity::accelerate(float vx, float vy)
+{
+    mVelocity.x += vx;
+    mVelocity.y += vy;
+}
+
+sf::Vector2f Entity::getVelocity() const
+{
+    return mVelocity;
+}
+
+int Entity::getHitpoints() const
+{
+    return mHitpoints;
+}
+
+void Entity::heal(int points)
+{
+    assert(points > 0);
+    mHitpoints += points;
+}
+
+void Entity::damage(int points)
+{
+    assert(points > 0);
+    mHitpoints -= points;
+}
+
+void Entity::destroy()
+{
+    mHitpoints = 0;
+}
+
+void Entity::remove()
+{
+    destroy();
+}
+
+bool Entity::isDestroyed() const
+{
+    return mHitpoints <= 0;
+}
+
+void Entity::updateCurrent(sf::Time dt, CommandQueue&)
+{
+    sf::Transformable::move(mVelocity * dt.asSeconds());
+}
