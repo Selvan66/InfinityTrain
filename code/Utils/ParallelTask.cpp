e51a887521dbc0795d3ad2650af829cd629763c1
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-04-05 12:31:58 +0200

    Add ParallelTask

---
+++ code/Utils/ParallelTask.cpp
@@ -0,0 +1,19 @@
+/** @file ParallelTask.cpp */
+#include <chrono>
+
+#include "Utils/ParallelTask.h"
+
+ParallelTask::ParallelTask(std::function<bool()> func)
+{ 
+    mFuture = std::async(std::launch::async, func);
+}
+
+ParallelTask::~ParallelTask()
+{
+    mFuture.get();
+}
+
+bool ParallelTask::isFinished() const
+{
+    return mFuture.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready;
+}
---
+++ code/Utils/ParallelTask.h
@@ -0,0 +1,16 @@
+/** @file ParallelTask.h */
+#pragma once
+
+#include <functional>
+#include <future>
+
+class ParallelTask
+{
+    public:
+        explicit ParallelTask(std::function<bool()> func);
+        ~ParallelTask();
+        bool isFinished() const;
+    private:
+        std::mutex mMutex;
+        std::future<bool> mFuture;
+};
