632bf20f96b091c912355f8e32ec765a21e00497
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-04-06 10:50:35 +0200

    update config

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 cd conan
-conan install -s build_type=Debug -s compiler.version=16 conanfile.txt
+conan install -s build_type=Debug -s compiler.version=16 -s arch=x86_64 conanfile.txt --build=missing
 cd ..
 mkdir build
 cd build
