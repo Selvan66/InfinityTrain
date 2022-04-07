9d2272a67ebb4213d8dc3d50a670ec86432f82f8
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-04-07 12:12:02 +0200

    Sprite Node

---
+++ code/Objects/Nodes/SpriteNode.cpp
@@ -0,0 +1,15 @@
+/** @file SpriteNode.cpp */
+#include "Objects/Nodes/SpriteNode.h"
+
+SpriteNode::SpriteNode(const sf::Texture& texture)
+: mSprite(texture)
+{ }
+
+SpriteNode::SpriteNode(const sf::Texture& texture, const sf::IntRect& textureRect)
+: mSprite(texture, textureRect) 
+{ }
+
+void SpriteNode::drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    target.draw(mSprite, states);
+}
---
+++ code/Objects/Nodes/SpriteNode.h
@@ -0,0 +1,15 @@
+/** @file SpriteNode.h */
+#pragma once
+
+#include "Objects/Nodes/SceneNode.h"
+
+class SpriteNode : public SceneNode
+{
+    public:
+        explicit SpriteNode(const sf::Texture& texture);
+        SpriteNode(const sf::Texture& texture, const sf::IntRect& textureRect);
+    private:
+        virtual void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const;
+    private:
+        sf::Sprite mSprite;
+};
