bf6ccdc2037353b7dd53904e092886ee82f089f6
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-04-18 10:05:09 +0200

    declaration Player

--- code/App/Player.h
+++ code/App/Player.h
@@ -27,6 +27,13 @@ class Player
     
     public:
         Player();
+        void handleEvent(const sf::Event& event, CommandQueue& commands);
+        void handleRealtimeInput(CommandQueue& commands);
+        void assignKey(Action action, sf::Keyboard::Key key);
+        sf::Keyboard::Key getAssignKey(Action action) const;
+    private:
+        void initializeActions();
+        static bool isRealtimeAction(Action action);
     private:
         std::unordered_map<Output, Action> mKeyBinding;
         std::unordered_map<Action, Command> mActionBinding;  
