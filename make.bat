424a80bc43cd938edc0265b45d0b12ae343bae01
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-05-08 13:13:48 +0200

    make conan setup with cmake

--- make.bat
+++
@@ -1,7 +0,0 @@
-cd conan
-conan install -s build_type=Debug -s compiler.version=16 -s arch=x86_64 conanfile.txt --build=missing
-cd ..
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build .
