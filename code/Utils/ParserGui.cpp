eec56b8c53492dfe6fd6080fce4d49720bcd9d9a
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-06-17 08:40:16 +0200

    parserGui
    gui file looks like windows ini file
    add typedef
    parser method v1

--- code/Utils/ParserGui.cpp
+++ code/Utils/ParserGui.cpp
@@ -27,9 +27,87 @@ void ParserGui::addConst(const std::string& name, double value)
     mConstants[name] = value;
 }
 
-std::unordered_map<std::string, std::unique_ptr<Component>>&& ParserGui::parse()
+ParserGui::GuiParsePtr ParserGui::parse()
 {
-    std::unordered_map<std::string, std::unique_ptr<Component>> ret;
-  
+    GuiParsePtr ret(new GuiParseType);
+    ComponentPtr component(nullptr);
+    std::string word(""), id(""), value("");
+    while (!mFile.eof())
+    {  
+        mFile >> word;
+        if (isComponent(word))
+        {  
+            if (component.get() != nullptr)
+            {
+                if (id == "")
+                    assert(true);   //TODO
+                ret->insert(std::make_pair(id, std::move(component)));
+                id = "";
+            }
+
+            component = getComponent(word);
+        }
+        else
+        {
+            mFile >> value;
+            if (isId(word))
+                id = value;
+            else
+                setProperties(component, word, value);
+        }
+    }
+
+    if (component.get() != nullptr)
+    {
+        if (id == "")
+            assert(true);   //TODO
+        ret->insert(std::make_pair(id, std::move(component)));
+    }
+
     return move(ret);
+}
+
+bool ParserGui::isComponent(const std::string& word) const
+{
+    if ((word[0] == '[') && (word.back() == ']'))
+        return true;
+    return false;
+}
+
+ParserGui::ComponentPtr ParserGui::getComponent(const std::string& word) const
+{
+    if (word == "[TextButton]")
+        return std::move(std::unique_ptr<TextButton>(new TextButton(mContext)));
+    else
+        assert(true);   //TODO
+}
+
+bool ParserGui::isId(const std::string& word) const
+{
+    if (word == "id")
+        return true;
+    return false;
+}
+
+sf::Vector2f ParserGui::parsePosition(const std::string& value) const
+{
+    return {500, 500};  //TODO
+}
+
+void ParserGui::setProperties(ComponentPtr& component, const std::string& propertie, const std::string& value) const
+{
+    if (propertie == "pos")
+    {
+        component->setPosition(parsePosition(value));
+    }
+    else if (propertie == "text")
+    {
+        if (dynamic_cast<TextButton*>(component.get()) == nullptr)
+            assert(true);   //TODO
+        static_cast<TextButton*>(component.get())->setText(value);
+    }
+    else
+    {
+        assert(true);   //TODO
+    }
 }
--- code/Utils/ParserGui.h
+++ code/Utils/ParserGui.h
@@ -12,10 +12,20 @@
 class ParserGui
 {
     public:
+        typedef std::unique_ptr<Component> ComponentPtr;
+        typedef std::unordered_map<std::string, ComponentPtr> GuiParseType;
+        typedef std::unique_ptr<GuiParseType> GuiParsePtr;
+    public:
         ParserGui(Context& context);
         bool loadFromFile(const std::string& filename);
         void addConst(const std::string& name, double value);
-        std::unordered_map<std::string, std::unique_ptr<Component>>&& parse();
+        GuiParsePtr parse();
+    private:
+        bool isComponent(const std::string& word) const;
+        ComponentPtr getComponent(const std::string& word) const;
+        bool isId(const std::string& word) const;
+        sf::Vector2f parsePosition(const std::string& value) const;
+        void setProperties(ComponentPtr& component, const std::string& propertie, const std::string& value) const;
     private:
         Context& mContext;
         std::stringstream mFile;
