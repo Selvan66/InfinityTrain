f757a2a798447fd2374c9cd311edeef43ab92537
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-06-20 08:40:35 +0200

    add safe casting function

--- code/Utils/Utility.h
+++ code/Utils/Utility.h
@@ -19,4 +19,12 @@ std::string timeToString(unsigned long long time);
 std::string toString(Player::Output key);
 Player::Output toKey(std::string key);
 
+
+template <typename T, typename P>
+T* safeCasting(P* cast)
+{
+    assert(dynamic_cast<T*>(cast) != nullptr);
+    return static_cast<T*>(cast);
+}
+
 }
