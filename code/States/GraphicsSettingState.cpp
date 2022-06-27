a3b58d97b18650412312597021443f0096831719
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-06-27 07:35:43 +0200

    add parser gui to graphics settings

--- code/States/GraphicsSettingState.cpp
+++ code/States/GraphicsSettingState.cpp
@@ -1,16 +1,15 @@
 /** @file GraphicsSettingState.cpp */
 #include "States/GraphicsSettingState.h"
 #include "Utils/Utility.h"
+#include "Gui/Checkbox.h"
+#include "Gui/TextSlider.h"
+#include "Gui/TextButton.h"
 
 GraphicsSettingState::GraphicsSettingState(StateStack& stack, Context& context)
 : State(stack, context)
-, mResolution("RESOLUTION", context.fonts.get(FontsID::PixelFont), 60)
-, mResolutionSlider(context)
 , mSaveResolution(context.settings.get<std::pair<int, int>>("Graphics", "Resolution"))
-, mFullscreen("FULLSCREEN", context.fonts.get(FontsID::PixelFont), 60)
 , mSaveFullscreen(context.settings.get<bool>("Graphics", "Fullscreen"))
-, mFullscreenCheckbox(context)
-, mOptionButtons()
+, mGui()
 {
     createGUI();
 }
@@ -18,33 +17,25 @@ GraphicsSettingState::GraphicsSettingState(StateStack& stack, Context& context)
 void GraphicsSettingState::draw()
 {
     auto& window = State::getContext().window;
-    window.draw(mResolution);
-    window.draw(mResolutionSlider);
-    window.draw(mFullscreen);
-    window.draw(mFullscreenCheckbox);
-    for (auto& button : mOptionButtons)
-		window.draw(button);
+    for (auto& component : *mGui)
+		window.draw(*component.second);
 }
 
 bool GraphicsSettingState::update(sf::Time)
 {
-    mResolutionSlider.update();
-    mFullscreenCheckbox.update();
-    for (auto& button : mOptionButtons)
-		button.update();
+    for (auto& component : *mGui)
+		component.second->update(); 
 
-    if (mFullscreenCheckbox.isSelected())
-        mResolutionSlider.setCurrentText("1920x1080");
+    if (Utility::safeCasting<Checkbox>(mGui->at("FullscreenBox").get())->isSelected())
+        Utility::safeCasting<TextSlider>(mGui->at("ResolutionSlider").get())->setCurrentText("1920x1080");
 
     return true;
 }
 
 bool GraphicsSettingState::handleEvent(const sf::Event& event)
 {
-    mResolutionSlider.handleEvent(event);
-    mFullscreenCheckbox.handleEvent(event);
-    for (auto& button : mOptionButtons)
-		button.handleEvent(event);
+    for (auto& component : *mGui)
+		component.second->handleEvent(event);
 	
     return true;
 }
@@ -52,37 +43,20 @@ bool GraphicsSettingState::handleEvent(const sf::Event& event)
 void GraphicsSettingState::createGUI()
 {
     auto& context = State::getContext();
-    const sf::Vector2f& windowsize = context.window.getView().getSize();
+    const sf::Vector2f& window_size = context.window.getView().getSize();
 
-    mResolutionSlider.addText("800x600");
-    mResolutionSlider.addText("1024x576");
-    mResolutionSlider.addText("1366x768");
-    mResolutionSlider.addText("1600x900");
-    mResolutionSlider.addText("1920x1080");
+    ParserGui& parser = context.gui.get(GuiFileID::GraphicsSetting);
+    parser.addConst("TEXT_HEIGHT", 100.f);
+    parser.addConst("TEXT_WIDTH", 270.f);
+    parser.addConst("WINDOW_WIDTH", window_size.x);
+	parser.addConst("WINDOW_HEIGHT", window_size.y);
+    mGui = parser.parse(context);
+
     std::string currentResolution = std::to_string(mSaveResolution.first) + "x" + std::to_string(mSaveResolution.second);
-    mResolutionSlider.setCurrentText(currentResolution);
+    Utility::safeCasting<TextSlider>(mGui->at("ResolutionSlider").get())->setCurrentText(currentResolution);
+    Utility::safeCasting<Checkbox>(mGui->at("FullscreenBox").get())->setSelection(mSaveFullscreen);
 
-    mFullscreenCheckbox.setSelection(mSaveFullscreen);
-
-    Utility::centerOrigin(mResolution);
-    mResolution.setOutlineThickness(2.f);
-	mResolution.setOutlineColor(sf::Color(0, 0, 0));
-    float textwidth = mResolution.getGlobalBounds().width * 3.f / 5.f;
-    float textheight = mResolution.getGlobalBounds().height + 50.f;
-    mResolution.setPosition(sf::Vector2f((windowsize.x / 2.f) - textwidth, windowsize.y / 2.f));
-    mResolutionSlider.setPosition((windowsize.x / 2.f) + textwidth, windowsize.y / 2.f);
-
-    Utility::centerOrigin(mFullscreen);
-    mFullscreen.setOutlineThickness(2.f);
-	mFullscreen.setOutlineColor(sf::Color(0, 0, 0));
-    mFullscreen.setPosition(sf::Vector2f((windowsize.x / 2.f) - textwidth, (windowsize.y / 2.f) + textheight));
-    mFullscreenCheckbox.setPosition((windowsize.x / 2.f) + textwidth,(windowsize.y / 2.f) + textheight);
-
-    mOptionButtons.emplace_back(context);
-    auto& backButton = mOptionButtons.back();
-    backButton.setText("BACK");
-    backButton.setPosition(sf::Vector2f(windowsize.x  * 2.f/8.f, windowsize.y * 4.f / 5.f));
-    backButton.setCallback([&]()
+    Utility::safeCasting<TextButton>(mGui->at("BackButton").get())->setCallback([&]()
     {
         context.settings.set<std::pair<int, int>>(mSaveResolution, "Graphics", "Resolution");
         context.settings.set<bool>(mSaveFullscreen, "Graphics", "Fullscreen");
@@ -91,31 +65,23 @@ void GraphicsSettingState::createGUI()
         this->requestStackPush(StatesID::SettingState);
     });
 
-    mOptionButtons.emplace_back(context);
-    auto& applyButton = mOptionButtons.back();
-    applyButton.setText("APPLY");
-    applyButton.setPosition(sf::Vector2f(windowsize.x  * 4.f/8.f, windowsize.y * 4.f / 5.f));
-    applyButton.setCallback([&]()
+    Utility::safeCasting<TextButton>(mGui->at("ApplyButton").get())->setCallback([&]()
     {
-        std::string applyResolution = mResolutionSlider.getCurrentText();
+        std::string applyResolution = Utility::safeCasting<TextSlider>(mGui->at("ResolutionSlider").get())->getCurrentText();
         std::string width = applyResolution.substr(0, applyResolution.find('x'));
         std::string height = applyResolution.substr(applyResolution.find('x') + 1, applyResolution.size());
         context.settings.set<std::pair<int, int>>(std::pair<int, int>(std::stoi(width), std::stoi(height)), "Graphics", "Resolution");
-        context.settings.set<bool>(mFullscreenCheckbox.isSelected(), "Graphics", "Fullscreen");
+        context.settings.set<bool>(Utility::safeCasting<Checkbox>(mGui->at("FullscreenBox").get())->isSelected(), "Graphics", "Fullscreen");
         context.applyGraphicSettings();
     });
 
-    mOptionButtons.emplace_back(context);
-    auto& saveButton = mOptionButtons.back();
-    saveButton.setText("APPLY & SAVE");
-    saveButton.setPosition(sf::Vector2f(windowsize.x  * 6.f/8.f, windowsize.y * 4.f / 5.f));
-    saveButton.setCallback([&]()
+    Utility::safeCasting<TextButton>(mGui->at("ApplySaveButton").get())->setCallback([&]()
     {
-        std::string applyResolution = mResolutionSlider.getCurrentText();
+        std::string applyResolution = Utility::safeCasting<TextSlider>(mGui->at("ResolutionSlider").get())->getCurrentText();
         std::string width = applyResolution.substr(0, applyResolution.find('x'));
         std::string height = applyResolution.substr(applyResolution.find('x') + 1, applyResolution.size());
         mSaveResolution = std::pair<int, int>(std::stoi(width), std::stoi(height));
-        mSaveFullscreen = mFullscreenCheckbox.isSelected();
+        mSaveFullscreen = Utility::safeCasting<Checkbox>(mGui->at("FullscreenBox").get())->isSelected();
         context.settings.set<std::pair<int, int>>(mSaveResolution, "Graphics", "Resolution");
         context.settings.set<bool>(mSaveFullscreen, "Graphics", "Fullscreen");
         context.applyGraphicSettings();
--- code/States/GraphicsSettingState.h
+++ code/States/GraphicsSettingState.h
@@ -2,9 +2,7 @@
 #pragma once
 
 #include "States/State.h"
-#include "Gui/TextSlider.h"
-#include "Gui/Checkbox.h"
-#include "Gui/TextButton.h"
+#include "Utils/ParserGui.h"
 
 class GraphicsSettingState : public State
 {
@@ -16,13 +14,8 @@ class GraphicsSettingState : public State
     private:
         void createGUI();
     private:
-        sf::Text mResolution;
         std::pair<int, int> mSaveResolution;
-        TextSlider mResolutionSlider;
-
-        sf::Text mFullscreen;
         bool mSaveFullscreen;
-        Checkbox mFullscreenCheckbox;
 
-        std::list<TextButton> mOptionButtons; 
+		ParserGui::GuiParsePtr mGui;
 };
