ee2f8e85308d2ab9fac4f7b51cd1aa625ff85a1f
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-07-03 08:49:29 +0200

    update setting state with new base state class

--- code/States/SettingState.cpp
+++ code/States/SettingState.cpp
@@ -1,69 +1,35 @@
 /** @file SettingState.cpp */
 #include "States/SettingState.h"
-#include "Utils/Utility.h"
 #include "Gui/TextButton.h"
 
 SettingState::SettingState(StateStack& stack, Context& context)
 : State(stack, context)
-, mGui()
 {
-    createButtons();
+    State::loadGuiParser(GuiFileID::Setting);
+    applyGuiFunctions();
 }
 
-void SettingState::draw()
+void SettingState::applyGuiFunctions()
 {
-    auto& window = State::getContext().window;
-    for (auto& component : *mGui)
-		window.draw(*component.second);
-}
-
-bool SettingState::update(sf::Time )
-{
-    for (auto& component : *mGui)
-		component.second->update();
-	
-    return true;
-}
-
-bool SettingState::handleEvent(const sf::Event& event)
-{
-    for (auto& component : *mGui)
-		component.second->handleEvent(event);
-	
-    return true;
-}
-
-void SettingState::createButtons()
-{
-    auto& context = State::getContext();
-    const sf::Vector2f& window_size = context.window.getView().getSize();
-    const float button_height = 65.f;
-    
-    ParserGui& parser = context.gui.get(GuiFileID::Setting);
-    parser.addConst("BUTTON_HEIGHT", button_height);
-    parser.addConst("WINDOW_WIDTH", window_size.x);
-	parser.addConst("WINDOW_HEIGHT", window_size.y);
-    mGui = parser.parse(context);
-	
-	Utility::safeCasting<TextButton>(mGui->at("GraphicsButton").get())->setCallback([this]()
+	State::getGuiComponent<TextButton>("GraphicsButton").setCallback([this]()
 	{
 		this->requestStackPop();
         this->requestStackPush(StatesID::GraphicsSettingState);
 	});
 	
-	Utility::safeCasting<TextButton>(mGui->at("ControlButton").get())->setCallback([this]()
+    State::getGuiComponent<TextButton>("ControlButton").setCallback([this]()
 	{
 		this->requestStackPop();
         this->requestStackPush(StatesID::ControlSettingState);
 	});
 
-	Utility::safeCasting<TextButton>(mGui->at("AudioButton").get())->setCallback([this]()
+	State::getGuiComponent<TextButton>("AudioButton").setCallback([this]()
 	{
 		this->requestStackPop();
         this->requestStackPush(StatesID::AudioSettingState);
 	});
 
-	Utility::safeCasting<TextButton>(mGui->at("BackButton").get())->setCallback([this]()
+	State::getGuiComponent<TextButton>("BackButton").setCallback([this]()
     {
         this->requestStackPop();
         this->requestStackPush(StatesID::MenuOptionsState);
--- code/States/SettingState.h
+++ code/States/SettingState.h
@@ -2,17 +2,11 @@
 #pragma once
 
 #include "States/State.h"
-#include "Utils/ParserGui.h"
 
 class SettingState : public State
 {
     public:
 		SettingState(StateStack& stack, Context& context);
-		virtual void draw() override;
-		virtual bool update(sf::Time dt) override;
-		virtual bool handleEvent(const sf::Event& event) override;
 	private:
-		void createButtons();
-	private:
-		ParserGui::GuiParsePtr mGui;
+		void applyGuiFunctions();
 };
