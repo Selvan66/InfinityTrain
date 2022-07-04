b4bd48abd1dc92a329c56e61a3059e7c7e3039cd
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-07-04 08:28:31 +0200

    bad argument exception

---
+++ code/Utils/Exceptions/bad_argument.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Utils/Exceptions/base_exception.h"
+
+namespace Except
+{
+
+class bad_argument : public base_exception
+{
+    public:
+        bad_argument() : base_exception(name()) { }
+        virtual const char* name() const override { return "Bad Argument"; }
+};
+
+}
+
