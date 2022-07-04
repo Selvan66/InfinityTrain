3d4bcb28355d8b15b92b2681ad74c5cff86bed8a
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-07-04 08:28:20 +0200

    bad_function_call exception

---
+++ code/Utils/Exceptions/bad_function_call.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Utils/Exceptions/base_exception.h"
+
+namespace Except
+{
+
+class bad_function_call : public base_exception
+{
+    public:
+        bad_function_call() : base_exception(name()) { }
+        virtual const char* name() const override { return "Bad Function Call"; }
+};
+
+}
+
