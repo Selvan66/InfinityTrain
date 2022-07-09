18faa96fd33abee1c5bb444fa62ece70ccc26cea
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-07-09 22:20:54 +0200

    add pocket to backpack

--- code/Player/Backpack.cpp
+++ code/Player/Backpack.cpp
@@ -1,12 +1,25 @@
 /** @file Backpack.cpp */
 #include "Player/Backpack.h"
 
-Backpack::Backpack()
-: mBackpack()
+#include <iostream> //< for debug
+
+Backpack::Backpack(Context& context)
+: mBackpack
+{context, context, context, context, context, 
+context, context, context, context, context, 
+context, context, context, context, context, 
+context, context, context, context, context}
 , mDropQueue()
 {
     for (int i = 0; i < mSize; ++i)
-        mBackpack[i] = std::unique_ptr<Pickup>(nullptr);
+        mBackpack[i].setLeftClickCallback([this](){std::cout << "TEST" << std::endl;});
+    
+    for (int i = 0; i < mSize; ++i)
+        mBackpack[i].setRightClickCallback([this, i](){this->giveItemToDrop(i);});
+
+    for (int i = 0; i < 4; ++i)
+        for (int j = 0; j < 5; ++j)
+            mBackpack[i * 5 + j].setPosition((j * 40.f), (i * 40.f));
 }
 
 void Backpack::addItemToBackpack(std::unique_ptr<Pickup> item)
@@ -19,15 +32,14 @@ void Backpack::addItemToBackpack(std::unique_ptr<Pickup> item)
         index = 0;
     }
     
-    mBackpack[index] = std::move(item);
+    mBackpack[index].addItem(std::move(item));
 }
 
 void Backpack::giveItemToDrop(size_t index)
 {
-    if (mBackpack[index].get() == nullptr)
+    if (!mBackpack[index].isItem())
         return;
-    mDropQueue.push(std::move(mBackpack[index]));
-    mBackpack[index] = std::unique_ptr<Pickup>(nullptr);
+    mDropQueue.push(std::move(mBackpack[index].dropItem()));
 }
 
 void Backpack::drop(sf::Vector2f pos, SceneNode& node)
@@ -36,21 +48,41 @@ void Backpack::drop(sf::Vector2f pos, SceneNode& node)
     command.category = Category::Floor;
     while (!mDropQueue.empty())
     {
-        mDropQueue.back()->setPosition(pos);
-        node.attachChild(std::move(mDropQueue.back()));
+        mDropQueue.front()->setPosition(pos);
+        node.attachChild(std::move(mDropQueue.front()));
         mDropQueue.pop();
     }
+}
+
+void Backpack::handleEvent(const sf::Event& event)
+{
+    for (int i = 0; i < mSize; ++i)
+        mBackpack[i].handleEvent(event);
+}
+
+void Backpack::update()
+{
+    for (int i = 0; i < mSize; ++i)
+        mBackpack[i].update();
+}
+
+void Backpack::setPosition(sf::Vector2f position)
+{
+    for (int i = 0; i < mSize; ++i)
+        mBackpack[i].move(position);
 }
 
 size_t Backpack::getFirstFreeIndex() const
 {
     for (size_t i = 0; i < mSize; ++i)
-        if (mBackpack[i].get() == nullptr)
+        if (!mBackpack[i].isItem())
             return i;
     return -1;
 }
 
 void Backpack::draw(sf::RenderTarget &target, sf::RenderStates states) const 
 {
-    // ...
+    states.transform *= sf::Transformable::getTransform();
+    for (int i = 0; i < mSize; ++i)
+        target.draw(mBackpack[i], states);
 }
--- code/Player/Backpack.h
+++ code/Player/Backpack.h
@@ -4,22 +4,26 @@
 #include <array>
 #include <queue>
 
-#include "Objects/Nodes/Pickup.h"
+#include "Player/Pocket.h"
 
 class Backpack : public sf::Drawable, public sf::Transformable, public sf::NonCopyable
 {
     public:
-        Backpack();
+        Backpack(Context& context);
 
         void addItemToBackpack(std::unique_ptr<Pickup> item);
         void giveItemToDrop(size_t index);
         void drop(sf::Vector2f pos, SceneNode& node);
+
+        void handleEvent(const sf::Event& event);
+        void update();
+        void setPosition(sf::Vector2f position);
     private:
         size_t getFirstFreeIndex() const;
         
         virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
     private:
         static constexpr size_t mSize = 20;
-        std::array<std::unique_ptr<Pickup>, mSize> mBackpack;
+        std::array<Pocket, mSize> mBackpack;
         std::queue<std::unique_ptr<Pickup>> mDropQueue;
 };
