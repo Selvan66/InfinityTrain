39c45cb1ead7b641b5254a8056637eb4a1a62c69
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-07-11 09:51:47 +0200

    door

---
+++ code/Objects/Nodes/Door.cpp
@@ -0,0 +1,72 @@
+/** @file Door.cpp */
+#include "Objects/Nodes/Door.h"
+
+
+Door::Door(Context& context, bool isOpen)
+: Interactable()
+, mIsOpen(false)
+, mInteract(false)
+, mBackground({152, 82})
+, mAnimation()
+{
+    Interactable::setDistance(100.f);
+    Utility::centerOrigin(mBackground);
+
+    if (isOpen)
+    {
+        mAnimation.setTexture(context.textures.get(TexturesID::OpenDoors));
+        mAnimation.setNumFrames(2);
+        mIsOpen = true;
+        sf::Transformable::setRotation(180);
+    }
+    else
+    {
+        mAnimation.setTexture(context.textures.get(TexturesID::CloseDoors));
+        mAnimation.setNumFrames(3); 
+    }
+    mAnimation.setDuration(sf::seconds(2));
+    mAnimation.setRepeating(false);
+    mAnimation.setFrameSize({150, 82});
+    mAnimation.update(sf::seconds(0));
+}
+
+void Door::interact()
+{
+    if (mIsOpen)
+        mInteract = true;
+}
+
+sf::FloatRect Door::getBoundingRect() const 
+{
+    return sf::Transformable::getTransform().transformRect(mAnimation.getGlobalBounds());
+}
+
+void Door::open()
+{
+    mIsOpen = true;
+}
+
+void Door::close()
+{
+    mIsOpen = false;
+}
+
+bool Door::isInteract() const
+{
+    return mInteract;
+}
+
+void Door::updateCurrent(sf::Time dt, CommandQueue& commands)
+{
+    Interactable::updateCurrent(dt, commands);
+    if (mIsOpen)
+        mAnimation.update(dt);
+}
+
+void Door::drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const 
+{
+    target.draw(mBackground, states);
+    target.draw(mAnimation, states);
+}
+
+
---
+++ code/Objects/Nodes/Door.h
@@ -0,0 +1,25 @@
+/** @file Door.h */
+#pragma once
+
+#include "Objects/Nodes/Interactable.h"
+#include "Effects/Animation.h"
+
+class Door : public Interactable
+{
+    public:
+        Door(Context& context, bool isOpen);
+        void open();
+        void close();
+        bool isInteract() const;
+
+        virtual void interact() override;
+    protected:
+        virtual sf::FloatRect getBoundingRect() const override;
+        virtual void updateCurrent(sf::Time dt, CommandQueue& commands) override;
+        virtual void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const override;
+    private:
+        bool mIsOpen;
+        bool mInteract;
+        sf::RectangleShape mBackground;
+        Animation mAnimation;
+};
