787a9074e92ae8433478d667122de5d181633e3c
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-07-13 13:38:50 +0200

    add LvlContext

---
+++ code/Objects/Levels/LvlContext.h
@@ -0,0 +1,12 @@
+/** @file LvlContext.h */
+#pragma once
+
+#include "App/Context.h"
+#include "Player/PlayerInfo.h"
+
+struct LvlContext
+{
+    Context& context;
+    PlayerInfo playerInfo;
+    unsigned int numLevel;
+};
