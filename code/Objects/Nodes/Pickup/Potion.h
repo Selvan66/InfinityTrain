bee2afdaf9a28da3b202868dfdee3a2c247b972f
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-07-27 08:40:58 +0200

    potion declaration

---
+++ code/Objects/Nodes/Pickup/Potion.h
@@ -0,0 +1,14 @@
+/** @file Potion.h */
+#pragma once
+
+#include "Objects/Nodes/Pickup/Pickup.h"
+
+class Potion : public Pickup
+{
+    public:
+        Potion(Context& context, unsigned int value);
+
+        virtual void use(PlayerNode& player) override;
+    private:
+        unsigned int mValue;
+};
