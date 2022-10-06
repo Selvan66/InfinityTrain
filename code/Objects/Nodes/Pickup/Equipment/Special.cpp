ff667b6e1e4b3e7d1bf273cb0dc9dd33a27650cb
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-10-06 07:02:46 +0200

    special prototype

---
+++ code/Objects/Nodes/Pickup/Equipment/Special.cpp
@@ -0,0 +1,92 @@
+/** @file Special.cpp */
+#include "Objects/Nodes/Pickup/Equipment/Special.h"
+#include "Objects/Nodes/PlayerNode.h"
+
+static std::array<SpecialParam, Special::SpecialCount> specials = 
+{
+    {
+        { "Portal Gun", 3, sf::seconds(1.f), {{ }}, TexturesID::Knife, sf::IntRect(0, 0, 32, 32), Command() },
+    }
+}; 
+
+void initSpecialsActions(Context& context)
+{
+    specials[Special::PortalGun].action.category = Category::Player;
+    specials[Special::PortalGun].action.action = derivedAction<PlayerNode>([&](PlayerNode& player, sf::Time) {
+        player.setPosition(Utility::getMousePos(context.window));
+    });
+}
+
+Special::Special(Context& context, Type type)
+: Special(context, type, specials[type].ammos) 
+{ }
+
+Special::Special(Context& context, Type type, int ammos)
+: Pickup(context)
+, mType(type)
+, mUse(false)
+, mElapsed(specials[type].duration + sf::seconds(1))
+{
+    initSpecialsActions(context);   //TODO: move this to a better place
+
+    Command command;
+    command.category = Category::Player;
+    command.action = derivedAction<PlayerNode>([&](PlayerNode& player, sf::Time) {
+        player.pickup(create());
+        this->destroy();
+    });
+
+    Pickup::setCommand(command);
+    Pickup::setTexture(specials[type].texture);
+    Entity::setHitpoints(ammos);
+}
+
+unsigned int Special::getCategory() const 
+{
+    return Pickup::getCategory() | Category::Special;
+}
+
+bool Special::action(PlayerNode& player)
+{
+    player.pickup(create());
+    return true;
+}
+
+std::string Special::getDescription() const 
+{
+    std::stringstream ss;
+    ss << specials[mType].name << '\n';
+    ss << "Ammos: " << (Entity::getHitpoints() == INF ? "INF" : std::to_string(Entity::getHitpoints())) << '\n';
+    ss << "Cooldown: " << specials[mType].duration.asSeconds() << "s";
+
+    for (auto& stat : getStats())
+        ss << '\n' << Stats::toString(stat.first) << ": " << stat.second;
+    return ss.str();
+}
+
+std::unordered_map<Stats::Type, int> Special::getStats() const 
+{
+    return specials[mType].stats;
+}
+
+std::unique_ptr<Pickup> Special::create() const 
+{
+    return std::make_unique<Special>(getContext(), mType, Entity::getHitpoints());
+}
+
+void Special::updateCurrent(sf::Time dt, CommandQueue& commands)
+{
+    Pickup::updateCurrent(dt, commands);
+    mElapsed += dt;
+    if (mElapsed >= specials[mType].duration)
+    {
+        if (mUse)
+        {
+            mElapsed = sf::Time::Zero;
+            commands.push(specials[mType].action);
+            if (Entity::getHitpoints() != INF)
+                Entity::damage(1);
+        }
+    }
+    mUse = false;   
+}
---
+++ code/Objects/Nodes/Pickup/Equipment/Special.h
@@ -0,0 +1,44 @@
+/** @file Special.h */
+#pragma once
+
+#include "Objects/Nodes/Pickup/Pickup.h"
+
+class Special : public Pickup
+{
+    public:
+        enum Type
+        {
+            PortalGun,
+            SpecialCount,
+        };
+    public:
+        Special(Context& context, Type type);
+        Special(Context& context, Type type, int ammos);
+
+        void use();
+
+        virtual unsigned int getCategory() const override;
+        virtual bool action(PlayerNode& player) override;
+        virtual std::string getDescription() const override;
+        virtual std::unordered_map<Stats::Type, int> getStats() const override;
+    protected:
+        virtual std::unique_ptr<Pickup> create() const override;
+        virtual void updateCurrent(sf::Time dt, CommandQueue& commands) override;
+    private:
+        const Type mType;
+        bool mUse;
+        sf::Time mElapsed;
+};
+
+struct SpecialParam
+{
+    const char* name;
+    const int ammos;
+    const sf::Time duration;
+    const std::unordered_map<Stats::Type, int> stats;
+    const TexturesID texture;
+    const sf::IntRect textureRect;
+    Command action;
+};
+
+void initSpecialsActions(Context& context);
