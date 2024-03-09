f31fe38a5ea731f0ead899d44844af304be1dd9e
Selvan66 <rafikop.pl@gmail.com>
2024-03-09 19:11:21 +0100

    build with parameters

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
-conan install . --build=missing --output-folder build
-cmake --preset conan-release -DCMAKE_EXPORT_COMPILE_COMMANDS=1
-cmake --build build
+#!/bin/sh
+
+if [ "$#" -eq 0 ]; then
+  echo "No build type specified. Please specifiy 'debug' or 'release'"
+  exit 1
+fi
+
+if [ "$1" == "debug" ]; then
+  conan install . --build=missing --output-folder build --settings=build_type=Debug
+  cmake --preset conan-debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+  cmake --build build
+elif [ "$1" == "release" ]; then
+  conan install . --build=missing --output-folder build --settings=build_type=Release
+  cmake --preset conan-release -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+  cmake --build build
+else
+  echo "Wrong build type. Please specifiy 'debug' or 'release'"
+  exit 1
+fi
