d3357eb613ea67b9ca792d175d13af669ce78d3e
Selvan66 <rafikop.pl@gmail.com>
2024-07-01 23:27:45 +0200

    add -j8 flag

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ fi
 if [ "$1" == "debug" ]; then
   conan install . --build=missing --output-folder build --settings=build_type=Debug
   cmake --preset conan-debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1
-  cmake --build build
+  cmake --build build -j8
 elif [ "$1" == "release" ]; then
   conan install . --build=missing --output-folder build --settings=build_type=Release
   cmake --preset conan-release -DCMAKE_EXPORT_COMPILE_COMMANDS=1
-  cmake --build build
+  cmake --build build -j8
 else
   echo "Wrong build type. Please specifiy 'debug' or 'release'"
   exit 1
