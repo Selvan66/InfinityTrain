f44e923f830e4ceaf2c4ecb9e5f91a57223562ac
Selvan66 <rafikop.pl@gmail.com>
2024-07-02 02:20:40 +0200

    add logs to Entity

--- code/Objects/Nodes/Entity.cpp
+++ code/Objects/Nodes/Entity.cpp
@@ -1,6 +1,8 @@
 /** @file Entity.cpp */
 #include <cmath>
 
+#include "spdlog/spdlog.h"
+
 #include "Objects/Nodes/Entity.h"
 #include "Utils/Utility.h"
 
@@ -21,28 +23,43 @@ void Entity::accelerate(float vx, float vy) {
   mVelocity.y += vy;
 }
 
-void Entity::setHitpoints(int hitpoints) { mHitpoints = hitpoints; }
+void Entity::setHitpoints(int hitpoints) {
+  spdlog::debug("Entity::setHitpoints | Category {} set hp to {}",
+                static_cast<int>(getCategory()), hitpoints);
+  mHitpoints = hitpoints;
+}
 
 sf::Vector2f Entity::getVelocity() const { return mVelocity; }
 
 int Entity::getHitpoints() const { return mHitpoints; }
 
-void Entity::destroy() { mHitpoints = 0; }
+void Entity::destroy() {
+  spdlog::debug("Entity::destroy | Category {} destroy",
+                static_cast<int>(getCategory()));
+  mHitpoints = 0;
+}
 
 bool Entity::heal(unsigned int points) {
+  spdlog::debug("Entity::heal | Category {} heal by {}",
+                static_cast<int>(getCategory()), points);
   int old_hitpoints = mHitpoints;
   mHitpoints = std::min(mHitpoints + static_cast<int>(points), 100);
   return old_hitpoints != mHitpoints;
 }
 
-bool Entity::damageFromPos(int points, sf::Vector2f pos) {
+bool Entity::damageWithKnockback(int points, sf::Vector2f pos) {
   auto direction = Utility::unitVector(SceneNode::getWorldPosition() - pos);
   accelerate(direction * 1000.f);
   return damage(points);
 }
 
 bool Entity::damage(int points) {
-  assert(points >= 0);
+  if (points < 0) {
+    spdlog::error("Entity::damage | Damage cannot be negative | {}", points);
+    return false;
+  }
+  spdlog::debug("Entity::damage | Category {} damage by {}",
+                static_cast<int>(getCategory()), points);
   mHitpoints -= points;
   return true;
 }
